b2b47e00233cdff12e2a5f16177a6647bb5e45df
Arvind <sraj.arvind@gmail.com>
2016-03-05 16:23:53 +0530

    Added code to insert script info, location of services and container info from config into DB

--- database/containers_reset_db.py
+++ database/containers_reset_db.py
@@ -31,12 +31,15 @@ def insert_config_values(config):
     print "Inserting team info into database"
     query = """INSERT INTO teams (team_name, services_ports_low, services_ports_high)
             VALUES (%s, %s, %s)"""
+    teams = {}
     for team in config["teams"]:
         values = (team["name"], team["services_ports_low"],
                   team["services_ports_high"])
         cursor.execute(query, values)
         team["id"] = db_obj.insert_id()
+        teams[team["name"]] = team
 
+    config["teams"] = teams
     db_obj.commit()
     print "done"
 
@@ -45,6 +48,7 @@ def insert_config_values(config):
     query = """INSERT INTO services (name, internal_port, description, authors,
             flag_id_description, offset_external_port) VALUES (%s, %s, %s, %s, %s,
             %s)"""
+    services = {}
     for service in config["services"]:
         description = service.get("description", "")
         flag_id_description = service.get("flag_id_description", "")
@@ -53,10 +57,54 @@ def insert_config_values(config):
                   service["offset_external_port"])
         cursor.execute(query, values)
         service["id"] = db_obj.insert_id()
+        services[service["name"]] = service
+
+    config["services"] = services
+    db_obj.commit()
+    print "done"
+
+    # Insert scripts details from config
+    print "Inserting scripts info into database"
+    query = """INSERT INTO scripts (name, type, service_id, is_working) VALUES (%s,
+            %s, %s, %s)"""
+
+    for script in config["scripts"]:
+        is_working = script.get("is_working", 1)
+        service_id = config["services"][script["service"]]["id"]
+        values = (script["name"], script["type"], service_id, is_working)
+        cursor.execute(query, values)
 
     db_obj.commit()
     print "done"
 
+    # Insert location of service containers into DB
+    print "Inserting services_locations info in database"
+    query = """INSERT INTO services_locations (team_id, service_id, host_ip,
+            host_port) VALUES (%s, %s, %s, %s)"""
+
+    for location in config["services_location"]:
+        team_id = config["teams"][location["team"]]["id"]
+        service_id = config["services"][location["service"]]["id"]
+        values = (team_id, service_id, location["host_ip"], location["host_port"])
+        cursor.execute(query, values)
+
+    db_obj.commit()
+    print "done"
+
+    # Insert containers info in DB
+    print "Inserting container information into DB"
+    query = """INSERT INTO containers (name, service_id, team_id, registry_namespace,
+            type) VALUES (%s, %s, %s, %s, %s)"""
+
+    for container in config["containers"]:
+        team_id = config["teams"][container["team"]]["id"]
+        service_id = config["services"][container["service"]]["id"]
+        values = (container["name"], service_id, team_id, container["namespace"],
+                  container["type"])
+        cursor.execute(query, values)
+
+    db_obj.commit()
+    print "done"
     return
 
 
