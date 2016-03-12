af7a47b2e9b849445e8aec236410122fd9dc80d5
Arvind <sraj.arvind@gmail.com>
2016-03-12 15:17:03 +0530

    gamebot updates all service container requiring update before starting a new tick

--- database/gamebot.py
+++ database/gamebot.py
@@ -1,21 +1,34 @@
 import MySQLdb
 import MySQLdb.cursors
 
+import os
 import iso8601
 import time
 import sys
 import json
 import random
+import logging
+
+import docker
 
 from datetime import datetime, timedelta
 
-from settings import MYSQL_DATABASE_USER, MYSQL_DATABASE_PASSWORD, MYSQL_DATABASE_DB
+from settings import MYSQL_DATABASE_USER, MYSQL_DATABASE_PASSWORD, \
+    MYSQL_DATABASE_DB, REMOTE_DOCKER_DAEMON_PORT, \
+    DOCKER_DISTRIBUTION_SERVER, DOCKER_DISTRIBUTION_USER, \
+    DOCKER_DISTRIBUTION_PASS, DOCKER_DISTRIBUTION_EMAIL
 
 
 TICK_TIME_IN_SECONDS = 180
 
 NUMBER_OF_BENIGN_SCRIPTS = 2
 NUMBER_OF_GET_SET_FLAG_COMBOS = 1
+
+LOG_PATH = "/tmp/gamebot.log"
+
+logging.basicConfig(filename=LOG_PATH, level=logging.WARNING,
+                    format='%(asctime)s, %(name)s, %(levelname)s, %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S')
 
 
 def main():
@@ -38,6 +51,16 @@ def main():
     service_ids = get_service_ids(c)
 
     while True:
+        # Update containers that have been changed since last update
+        print "Finding service containers that need to be updated"
+        updated_containers = get_service_containers_needing_update(c)
+        if updated_containers:
+            print "%d containers need to be updated" % (len(updated_containers))
+            update_service_containers(db, updated_containers)
+            print "Updated %d containers" % (len(updated_containers))
+        else:
+            print "All containers are updated"
+
         # Create a new tick
         current = datetime.now()
 
@@ -79,6 +102,54 @@ def main():
         print "Sleeping for", seconds_to_sleep
         time.sleep(seconds_to_sleep)
         print "Awake"
+
+
+def get_service_containers_needing_update(cursor):
+    containers_list = []
+    cursor.execute("""select name, registry_namespace, image_name, team_id,
+                   service_id from containers where update_required = True and type =
+                   'Service'""")
+    containers = cursor.fetchall()
+
+    teams = {}
+    cursor.execute("""select id, team_name from teams""")
+    for row in cursor.fetchall():
+        teams[row["id"]] = row["team_name"]
+
+    services = {}
+    cursor.execute("""select id, name from services""")
+    for row in cursor.fetchall():
+        services[row["id"]] = row["name"]
+
+    cursor.execute("""select flags_storage_folder_host from game limit 1""")
+    result = cursor.fetchone()
+    flags_storage_folder = result["flags_storage_folder_host"]
+
+    for container in containers:
+        ret_container = {}
+        for key in container:
+            ret_container[key] = container[key]
+
+        cursor.execute("""select host_ip, host_port from services_locations where
+                       team_id = %s and service_id = %s limit 1""",
+                       (container["team_id"], container["service_id"]))
+        result = cursor.fetchone()
+        ret_container["host_ip"] = result["host_ip"]
+        ret_container["external_port"] = result["host_port"]
+
+        cursor.execute("""select internal_port, workdir from services where
+                       id = %s""", (container["service_id"]))
+        result = cursor.fetchone()
+        ret_container["internal_port"] = result["internal_port"]
+        ret_container["internal_flag_storage"] = result["workdir"]
+
+        team = teams[container["team_id"]]
+        service = services[container["service_id"]]
+        ret_container["external_flag_storage"] = os.path.join(flags_storage_folder,
+                                                              team, service)
+        containers_list.append(ret_container)
+
+    return containers_list
 
 
 def get_list_of_scripts_to_run(c, service_ids, num_benign_scripts):
@@ -155,6 +226,95 @@ def get_current_tick(c):
             seconds_left = 0
 
     return current_tick, seconds_left
+
+
+def update_service_containers(db, containers):
+    remote_clients = {}
+    max_client_connect_retries = 3
+    user = DOCKER_DISTRIBUTION_USER
+    password = DOCKER_DISTRIBUTION_PASS
+    email = DOCKER_DISTRIBUTION_EMAIL
+    server = DOCKER_DISTRIBUTION_SERVER
+    port_num = REMOTE_DOCKER_DAEMON_PORT
+    cursor = db.cursor()
+    for container in containers:
+        host_ip = container['host_ip']
+        if host_ip not in remote_clients:
+            url = "tcp://%s:%d" % (host_ip, port_num)
+            for _ in xrange(max_client_connect_retries):
+                remote_clients[host_ip] = docker.Client(base_url=url)
+                if remote_clients[host_ip].ping() == "OK":
+                    break
+            else:
+                logging.error("Unable to connect to docker instance on %s:%s" %
+                              (host_ip, port_num))
+                return
+
+        remote_client = remote_clients[host_ip]
+        logging.info("Updating service container of service %d team %d on host %s" %
+                     (container["service_id"], container["team_id"], host_ip))
+
+        try:
+            # Stop container
+            remote_client.stop(container["name"])
+            logging.info("Stopped container %s" % (container["name"],))
+            # Delete container
+            remote_client.remove_container(container["name"])
+            logging.info("Removed container %s" % (container["name"]))
+        except docker.errors.NotFound:
+            logging.warning("No container %s found on %s" %
+                            (container["name"], host_ip))
+
+        # Update image
+        try:
+            remote_client.login(username=user, password=password, email=email,
+                                registry=server)
+        except docker.errors.APIError, e:
+            logging.error("Login to registry %s as %s failed" % (server, user))
+            logging.error(e)
+            continue
+
+        image_url = '/'.join([server, container['registry_namespace'],
+                              container['image_name']])
+        try:
+            output = list(remote_client.pull(image_url, stream=True))
+        except docker.errors.APIError, e:
+            logging.error("APIError when pulling image %s failed" % (image_url))
+            logging.error(e)
+            continue
+
+        last_line_json = json.loads(output[-1])
+        if 'Downloaded new image' not in last_line_json['status'] and \
+           'Image is up to date' not in last_line_json['status']:
+            # Error!
+            logging.error("Pulling image %s failed. Docker client output below." %
+                          (image_url))
+            logging.error("%s" % (output))
+            continue
+
+        # Create and start container
+        port_mapping = {container['internal_port']: container['external_port']}
+        folder_mappings = {
+            container['external_flag_storage']: {
+                'bind': container['internal_flag_storage'],
+                'mode': 'rw'
+                }
+            }
+
+        container_config = remote_client.create_host_config(binds=folder_mappings,
+                                                            port_bindings=port_mapping)
+
+        remote_client.create_container(image=image_url,
+                                       ports=[container['internal_port']],
+                                       name=container["name"],
+                                       host_config=container_config)
+        remote_client.start(container["name"])
+        cursor.execute("""update containers set update_required = False where
+                       team_id=%s and service_id=%s and type='Service'""",
+                       (container["team_id"], container["service_id"]))
+        db.commit()
+
+    return
 
 
 if __name__ == "__main__":
