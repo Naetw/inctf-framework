f2fa82a39c5e1a8ef5768aeebb37425509e6d7f9
Arvind <sraj.arvind@gmail.com>
2016-04-05 13:52:41 +0530

    Added remaining end points: /scores, /services and /services_status.

--- dashboard/web.py
+++ dashboard/web.py
@@ -8,6 +8,7 @@ import json
 import yaml
 from flask import Flask, render_template
 from flask_httpauth import HTTPBasicAuth
+import redis
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -15,6 +16,11 @@ config_file = "config.yml"
 fh = open(config_file)
 config = yaml.load(fh.read())
 fh.close()
+redis_client = redis.StrictRedis(host='localhost', port='6379', db=0)
+
+team_ids = {}
+for team in config['teams']:
+    team_ids[config['teams'][team]['name']] = team
 
 
 @auth.get_password
@@ -35,6 +41,34 @@ def index():
 @auth.login_required
 def get_config():
     return json.dumps({'ctf_name': config['name'], 'team_name': auth.username()})
+
+
+@app.route('/scores')
+def get_scores():
+    return redis_client.get('ctf_scores')
+
+
+@app.route('/services')
+@auth.login_required
+def get_services():
+    return redis_client.get('ctf_services')
+
+
+@app.route('/services_status')
+@auth.login_required
+def get_services_status():
+    status = json.loads(redis_client.get('ctf_services_status'))
+    result = {}
+    current_team_id = team_ids[auth.username()]
+    for state in status:
+        if state['team_id'] == current_team_id:
+            print "name: %s, team_id: %d" % (auth.username(), current_team_id)
+            for entry in state['services']:
+                result[entry['service_id']] = entry['state']
+
+            break
+
+    return json.dumps(result)
 
 
 if __name__ == "__main__":
