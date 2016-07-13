cef33e0e74feadfea999ee189dcb9201a0de9bb9
Arvind <sraj.arvind@gmail.com>
2016-07-13 17:19:17 +0530

    Pass port number of redis server as integer

--- dashboard/web.py
+++ dashboard/web.py
@@ -16,7 +16,7 @@ config_file = "config.json"
 fh = open(config_file)
 config = json.load(fh)
 fh.close()
-redis_client = redis.StrictRedis(host='localhost', port='6379', db=0)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 team_ids = {}
 for team in config['teams']:
